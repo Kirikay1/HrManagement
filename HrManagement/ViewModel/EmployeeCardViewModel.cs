466b4cf14208d061decfc11c57ae0ce4d5bda351
Maxim <maksim.timoshin.045@gmail.com>
2026-01-30 14:40:27 +0300

    feat: add data output

---
+++ HrManagement/ViewModel/EmployeeCardViewModel.cs
@@ -0,0 +1,46 @@
+﻿using HrManagement.Model;
+using System.Data.Entity;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace HrManagement.ViewModel
+{
+    public class HrManagementPageViewModel : ViewModelBase
+    {
+        private ObservableCollection<EmployeeCardViewModel> employeeCards;
+
+        public HrManagementPageViewModel()
+        {
+            LoadEmployees();
+        }
+
+        public ObservableCollection<EmployeeCardViewModel> EmployeeCards
+        {
+            get => employeeCards;
+            private set
+            {
+                employeeCards = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void LoadEmployees()
+        {
+            var employees = AppData.db.Employee
+                .Include(employee => employee.Department)
+                .Include(employee => employee.Position)
+                .ToList();
+
+            EmployeeCards = new ObservableCollection<EmployeeCardViewModel>(
+                employees.Select(employee => new EmployeeCardViewModel
+                {
+                    DepartmentName = employee.Department?.NameDepartment,
+                    PositionName = employee.Position?.NamePosition,
+                    FullName = employee.FullName,
+                    WorkPhone = employee.WorkPhone,
+                    Email = employee.Email,
+                    EmployeeOffice = employee.EmployeeOffice
+                }));
+        }
+    }
+}
---
+++ HrManagement/ViewModel/HrManagementPageViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace HrManagement.ViewModel
+{
+    public class EmployeeCardViewModel : ViewModelBase
+    {
+        private string departmentName;
+        private string positionName;
+        private string fullName;
+        private string workPhone;
+        private string email;
+        private string employeeOffice;
+
+        public string DepartmentName
+        {
+            get => departmentName;
+            set
+            {
+                departmentName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string PositionName
+        {
+            get => positionName;
+            set
+            {
+                positionName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string FullName
+        {
+            get => fullName;
+            set
+            {
+                fullName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string WorkPhone
+        {
+            get => workPhone;
+            set
+            {
+                workPhone = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Email
+        {
+            get => email;
+            set
+            {
+                email = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string EmployeeOffice
+        {
+            get => employeeOffice;
+            set
+            {
+                employeeOffice = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
