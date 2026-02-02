f3bf4dec709e3b4e432b03b8c962838ea72e9c41
Maxim <maksim.timoshin.045@gmail.com>
2026-02-02 08:09:37 +0300

    feat: add function button departaments

--- HrManagement/ViewModel/HrManagementPageViewModel.cs
+++ HrManagement/ViewModel/HrManagementPageViewModel.cs
@@ -1,17 +1,21 @@
-﻿using HrManagement.Model;
+﻿﻿using HrManagement.Model;
 using System.Collections.ObjectModel;
 using System.Data.Entity;
+using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Input;
 
 namespace HrManagement.ViewModel
 {
     public class HrManagementPageViewModel : ViewModelBase
     {
+        private readonly List<EmployeeCardViewModel> allEmployeeCards = new List<EmployeeCardViewModel>();
         private ObservableCollection<EmployeeCardViewModel> employeeCards;
 
         public HrManagementPageViewModel()
         {
             LoadEmployees();
+            FilterByDepartmentCommand = new RelayCommand(parameter => FilterEmployeesByDepartment(parameter as string));
         }
 
         public ObservableCollection<EmployeeCardViewModel> EmployeeCards
@@ -24,6 +28,8 @@ namespace HrManagement.ViewModel
             }
         }
 
+        public ICommand FilterByDepartmentCommand { get; }
+
         private void LoadEmployees()
         {
             var employees = AppData.db.Employee
@@ -31,16 +37,33 @@ namespace HrManagement.ViewModel
                 .Include(employee => employee.Position)
                 .ToList();
 
-            EmployeeCards = new ObservableCollection<EmployeeCardViewModel>(
-                employees.Select(employee => new EmployeeCardViewModel
-                {
-                    DepartmentName = employee.Department?.NameDepartment,
-                    PositionName = employee.Position?.NamePosition,
-                    FullName = employee.FullName,
-                    WorkPhone = employee.WorkPhone,
-                    Email = employee.Email,
-                    EmployeeOffice = employee.EmployeeOffice
-                }));
+            allEmployeeCards.Clear();
+            allEmployeeCards.AddRange(employees.Select(employee => new EmployeeCardViewModel
+            {
+                DepartmentName = employee.Department?.NameDepartment,
+                PositionName = employee.Position?.NamePosition,
+                FullName = employee.FullName,
+                WorkPhone = employee.WorkPhone,
+                Email = employee.Email,
+                EmployeeOffice = employee.EmployeeOffice
+            }));
+
+            EmployeeCards = new ObservableCollection<EmployeeCardViewModel>(allEmployeeCards);
+        }
+
+        public void FilterEmployeesByDepartment(string departmentName)
+        {
+            if (string.IsNullOrWhiteSpace(departmentName))
+            {
+                EmployeeCards = new ObservableCollection<EmployeeCardViewModel>(allEmployeeCards);
+                return;
+            }
+
+            var filtered = allEmployeeCards
+                .Where(employee => string.Equals(employee.DepartmentName, departmentName, System.StringComparison.OrdinalIgnoreCase))
+                .ToList();
+
+            EmployeeCards = new ObservableCollection<EmployeeCardViewModel>(filtered);
         }
     }
 }
